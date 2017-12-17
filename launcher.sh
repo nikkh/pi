863290f2b731a050e76bac350ecee28e274e6d83
nikkh <nick-9979@outlook.com>
2017-12-17 18:30:20 +0000

    Updated launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,5 +3,10 @@
 # navigate home, then back here and then home again
 cd /
 cd home/pi/Documents/pi
-python ipaddr.py
+echo "install service bus" > /home/pi/logs/startup.log
+pip install azure.servicebus > /home/pi/logs/install.log
+echo "publish my ip address" > /home/pi/logs/startup.log
+python ipaddr.py > /home/pi/logs/ipaddr.log
+echo "monitor for pi control messages" > /home/pi/logs/startup.log
+python piservicebusclient.py > /home/pi/logs/piservicebusclient.log
 cd /
